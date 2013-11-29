fb476465f89ddb93e6a9d64046c1e45e06a130a1
Rob McDiarmid <rob_mcdiarmid@otpp.com>
2013-11-29 10:25:10 -0500

    Removed unnecessary code

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,6 @@
 module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-replace');
     grunt.loadNpmTasks('grunt-contrib-compass');
 
     // Project configuration.
@@ -10,36 +9,13 @@ module.exports = function(grunt) {
 
         watch: {
             scripts: {
-                files: ['**/*.js', '**/*.scss', '**/*.html'],
-                tasks: ['replace', 'compass']
-            },
-            options: {
-                atBegin: true,
-                dateFormat: function(time) {
-                    grunt.log.writeln('Complete in ' + time + 's at ' + new Date().toLocaleTimeString());
-                    grunt.log.writeln('Waiting for changes...');
-                }
-            }
-        },
-
-        replace: {
-            dist: {
-                options: {
-                    patterns: [
-                        {
-                            match: 'hash',
-                            replacement: '<%= new Date().getTime() %>'
-                        }
-                    ]
-                },
-                files: [
-                    {src: ['public/index.html'], dest: 'public/indexCacheBusted.html'}
-                ]
+                files: ['**/*.scss'],
+                tasks: ['compass']
             }
         },
 
         compass: {
-            dist: {
+            compile: {
                 options: {
                     sassDir: 'public/stylesheets/sass',
                     cssDir: 'public/stylesheets/css'
@@ -49,13 +25,6 @@ module.exports = function(grunt) {
 
     });
 
-
-
-
-    grunt.registerTask('watchDev', ['watch']);
-    grunt.registerTask('prod', ['replace']);
-
-    // Default task(s).
-    grunt.registerTask('default', ['watchDev']);
+    grunt.registerTask('default', ['watch']);
 
 };
--- server.js
+++ server.js
@@ -1,47 +1,16 @@
-
-/**
- * Module dependencies.
- */
+// Module dependencies.
 var express = require('express');
 var http = require('http');
-var path = require('path');
 
 var server = express();
 
-// all environments
-server.set('port', process.env.PORT || 3000);
-
-server.use(express.logger('dev'));
-//Lets you use server.put() and server.delete(). Without this you can only user server.post() and server.get().
-server.use(express.methodOverride());
-//Having this line before any express.static lines will mean that routes set up with server.get, etc. will override
-//static files with the same name. http://stackoverflow.com/questions/12695591/node-js-express-js-how-does-app-router-work
-server.use(server.router);
-//I think this sets the icon seen beside the page title in a browser tab. You can pass a path into favicon.
-server.use(express.favicon());
-//This is needed to use the body of a post request.
-server.use(express.bodyParser());
 //Make the public folder the context root
-server.use(express.static(path.join(__dirname, 'public')));
+server.use(express.static('public'));
 
-// development only
-if (server.get('env') == 'development') {
-    server.use(express.errorHandler());
-}
-
-//Setup endpoints
+//Serve index.html when a user requests '/'
 server.get('/', function(req, res) {
-    res.sendfile("public/indexCacheBusted.html");
-});
-
-server.get('/api/someResource/:someParam', function (req, res) {
-    res.header("Cache-Control", "max-age=0,no-cache,no-store");
-
-    // do something with req.params.someParam
-    res.send("some response");
+    res.sendfile("public/index.html");
 });
 
 //Start Server
-http.createServer(server).listen(server.get('port'), function(){
-    console.log('Express server listening on port ' + server.get('port'));
-});
+http.createServer(server).listen(3000);
